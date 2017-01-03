ee2f9254187c6075eafc34207bb7c8f67bc7a0f5
Alex Defreitas <alexandreadef@gmail.com>
2017-01-03 17:41:37 -0500

    Select state from dropdown

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,22 +1,13 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <select v-model="selected">
+      <option v-for="state in states" v-bind:value="state">
+        {{ state }}
+      </option>
+    </select>
+    <h4>or, use my location</h4>
+    <p>You chose {{ selected }}</p>
   </div>
 </template>
 
@@ -25,10 +16,19 @@ export default {
   name: 'hello',
   data() {
     return {
-      msg: 'Welcome to Your Vue.js App',
+      msg: 'Find some yummy  food!',
+      selected: 'Rhode Island',
+      states: [
+        'Rhode Island',
+        'Texas',
+        'Maine',
+        'Massachusetts',
+        'New York',
+      ],
     };
   },
 };
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
