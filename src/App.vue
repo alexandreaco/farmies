8e1c16e01dbf1db6be811073430dee307c75a79b
Alexandrea Defreitas <alexandreadef@gmail.com>
2017-01-06 16:16:16 -0500

    Adds some styles

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
+    <header>
      <router-link to="/"><h1>Farmies</h1></router-link>
+   </header>
 
      <router-view class="view"></router-view>
    </div>
@@ -14,12 +16,29 @@ export default {
 </script>
 
 <style>
+body {
+  background-color: #F7F7F7;
+  margin: 0;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 0 auto;
+  max-width: 900px;
+}
+
+header {
+  border-bottom: 1px solid #ddd;
+  padding: 10px 5px;
+  text-align: right;
+}
+
+h1 {
+  font-size: 14px;
+  margin: 0 10px;
 }
 </style>
--- src/components/Farms.vue
+++ src/components/Farms.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="farms">
-    <h2>Showing farms in {{ state }}</h2>
-    <ul class="toggle">
-      <li><router-link to="map">Map</router-link></li>
-      <li><router-link to="list">List</router-link></li>
-    </ul>
+    <div class="title">
+      <h2>Showing farms in {{ state }}</h2>
+      <ul class="toggle">
+        <li><router-link to="map">Map</router-link></li>
+        <li><router-link to="list">List</router-link></li>
+      </ul>
+    </div>
     <router-view></router-view>
   </div>
 </template>
@@ -23,5 +25,26 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.farms {
+  margin-top: 20px;
+}
 
+.title {
+  display: flex;
+  justify-content: space-between;
+  margin: 5px 10px;
+}
+
+h2 {
+  margin: 0;
+  font-size: 18px;
+}
+
+.toggle {
+  display: flex;
+  justify-content: space-between;
+  list-style: none;
+  margin: 0;
+  width: 80px;
+}
 </style>
